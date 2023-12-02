532e3cc5cb18ca13bcb3bc8a1b7e3bef46f86ba8
satyam kumar <101463844+satyam-64136@users.noreply.github.com>
2023-12-02 11:39:58 +0530

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -20,7 +20,7 @@ function moveButton() {
     noButton.style.left = `${randomPosition.left}px`;
 }
 
-setInterval(moveButton, 800);
+setInterval(moveButton, 400);
 
 function changeContent() {
     document.getElementById('mainHeading').innerText = "YAY, see you soon...ɷ◡ɷ";
